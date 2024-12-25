a2ad4f1b317822cab6fe8f299550af3368a20b92
WaddleOs <waddleservice@gmail.com>
2024-12-25 18:43:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('canvaProButton').addEventListener('click', function() {
+    window.location.href = 'https://www.canva.com/brand/join?token=YlXvEwevSHmDSYeMvZWLSw&referrer=team-invite?'; // Replace this URL with your desired link
+});
+
+document.getElementById('telegramButton').addEventListener('click', function() {
+    window.location.href = 'https://t.me/canvaproteamss'; // Replace with your Telegram group link
+});
